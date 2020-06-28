845162d2e165e3bd2610894db9df9c5c99d86442
Gia Ly <gialy@Hangs-MacBook-Pro.local>
2020-06-28 23:30:47 +0700

    [Implement] Setup Project

---
+++ src/components/label.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Label = (props) => {
+
+    return (
+        <>
+           { props.title } 
+        </>
+    );
+};
+
+export default Label;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
+import Home from './pages/home';
 
 ReactDOM.render(
   <div>
-    My Blog
+    <Home />
   </div>
   ,document.getElementById('root')
 );
