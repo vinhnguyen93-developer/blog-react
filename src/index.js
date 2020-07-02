31268b402e7b3c12245621643f3003faae8139ed
Gia Ly <gialy@Hangs-MacBook-Pro.local>
2020-07-02 22:01:55 +0700

    [Update] font, reset css

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
+import "./index.css";
+
 import Home from './pages/home';
 
 ReactDOM.render(
