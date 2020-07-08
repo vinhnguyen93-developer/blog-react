233bf4959425bb20951c5bc1110ccf175d9bc18f
Vinhnguyen93-developer <developerthienkim@gmail.com>
2020-07-08 21:00:40 +0700

    [Implement] Categories Component

---
+++ src/components/categories/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './styles.css';
+
+/*
+  title: String,
+  itemClassName: String,
+  className: String,
+  items: Array<{
+      title: String,
+      count: Number
+  }>
+*/
+
+const Categories = (props) => {
+  return (
+    <div className={ props.className || "sidebar-box" }>
+      <h3 className="sidebar-heading">{props.title}</h3>
+      <ul className="categories">
+        { props.items.map((item, index) => {
+          return <li className={props.itemClassName} key={index}>{item.title}<span>({item.count})</span></li>;
+        })}
+      </ul>
+    </div>
+  );
+}
+
+export default Categories;
