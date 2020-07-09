8b776f312b3cd2d227df7c7db139ddb0e4754a09
Gia Ly <gialy@Hangs-MacBook-Pro.local>
2020-07-09 09:14:20 +0700

    [Update] withLayout: Responsive

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -7,28 +7,56 @@ const Home = (props) => {
     return (
         <div>
             <Feed 
-                imageUrl="https://lh3.googleusercontent.com/proxy/TUWfAyJMZ4L18CwP6jgCnukVvzBWJaQ4EvokBPltwd0SUyZbnP59WklVHrQ56zAMOYxuRAhwN1crwaHNksLx_fO3njfaMhCjwT9NEuCqMFEiWRsidrswGafOms2ThDoK2g"
+                imageUrl="https://dulichhalongquangninh.com/wp-content/uploads/2016/07/hinh-anh-du-lich-ha-long.jpg"
                 title="Vinh Ha Long"
                 createdDate="17/05/2020"
                 topic="Travel"
                 desc="Du lich Vinh Ha Long"
             />
             <Feed 
-                imageUrl="https://lh3.googleusercontent.com/proxy/TUWfAyJMZ4L18CwP6jgCnukVvzBWJaQ4EvokBPltwd0SUyZbnP59WklVHrQ56zAMOYxuRAhwN1crwaHNksLx_fO3njfaMhCjwT9NEuCqMFEiWRsidrswGafOms2ThDoK2g"
+                imageUrl="https://dulichhalongquangninh.com/wp-content/uploads/2016/07/hinh-anh-du-lich-ha-long.jpg"
                 title="Vinh Ha Long"
                 createdDate="17/05/2020"
                 topic="Travel"
                 desc="Du lich Vinh Ha Long"
             />
             <Feed 
-                imageUrl="https://lh3.googleusercontent.com/proxy/TUWfAyJMZ4L18CwP6jgCnukVvzBWJaQ4EvokBPltwd0SUyZbnP59WklVHrQ56zAMOYxuRAhwN1crwaHNksLx_fO3njfaMhCjwT9NEuCqMFEiWRsidrswGafOms2ThDoK2g"
+                imageUrl="https://dulichhalongquangninh.com/wp-content/uploads/2016/07/hinh-anh-du-lich-ha-long.jpg"
                 title="Vinh Ha Long"
                 createdDate="17/05/2020"
                 topic="Travel"
                 desc="Du lich Vinh Ha Long"
             />
             <Feed 
-                imageUrl="https://lh3.googleusercontent.com/proxy/TUWfAyJMZ4L18CwP6jgCnukVvzBWJaQ4EvokBPltwd0SUyZbnP59WklVHrQ56zAMOYxuRAhwN1crwaHNksLx_fO3njfaMhCjwT9NEuCqMFEiWRsidrswGafOms2ThDoK2g"
+                imageUrl="https://dulichhalongquangninh.com/wp-content/uploads/2016/07/hinh-anh-du-lich-ha-long.jpg"
+                title="Vinh Ha Long"
+                createdDate="17/05/2020"
+                topic="Travel"
+                desc="Du lich Vinh Ha Long"
+            />
+            <Feed 
+                imageUrl="https://dulichhalongquangninh.com/wp-content/uploads/2016/07/hinh-anh-du-lich-ha-long.jpg"
+                title="Vinh Ha Long"
+                createdDate="17/05/2020"
+                topic="Travel"
+                desc="Du lich Vinh Ha Long"
+            />
+            <Feed 
+                imageUrl="https://dulichhalongquangninh.com/wp-content/uploads/2016/07/hinh-anh-du-lich-ha-long.jpg"
+                title="Vinh Ha Long"
+                createdDate="17/05/2020"
+                topic="Travel"
+                desc="Du lich Vinh Ha Long"
+            />
+            <Feed 
+                imageUrl="https://dulichhalongquangninh.com/wp-content/uploads/2016/07/hinh-anh-du-lich-ha-long.jpg"
+                title="Vinh Ha Long"
+                createdDate="17/05/2020"
+                topic="Travel"
+                desc="Du lich Vinh Ha Long"
+            />
+            <Feed 
+                imageUrl="https://dulichhalongquangninh.com/wp-content/uploads/2016/07/hinh-anh-du-lich-ha-long.jpg"
                 title="Vinh Ha Long"
                 createdDate="17/05/2020"
                 topic="Travel"
