e5d191119d346a54ec4df1344475771c9b674587
vinhnguyen93-developer <developerthienkim@gmail.com>
2020-09-20 16:27:59 +0700

    [Implement] Create method login and register

--- src/pages/auth/login.js
+++ src/pages/auth/login.js
@@ -1,18 +1,42 @@
-import React from "react";
+import React, {useRef} from "react";
+import ApiService from "../../shared/services/apiService";
 import "./styles.css";
 
 const Login = (props) => {
+    const emailRef = useRef();
+    const passwordRef = useRef();
+
+    const handleSubmitForm = (e) => {
+        e.preventDefault();
+        let data = {
+            email: emailRef.current.value,
+            password: passwordRef.current.value
+        }
+
+        ApiService.axios(
+            "/auth/login",
+            {
+                method: "POST",
+                payload: data
+            }
+        ).then(({data}) => {
+            localStorage.setItem("token", data.data.token)
+            window.location.assign("/")
+
+        }).catch(err => console.log(err))
+    }
+
     return (
         <div className="login-form">
             <div className="container">
                 <div className="col">
                     <h2>Login</h2>
-                    <form className="form">
-                        <input className="text-box" type="text" placeholder="Email" />
-                        <input className="text-box" type="password" placeholder="Password" />
+                    <form className="form" onSubmit={handleSubmitForm}>
+                        <input className="text-box" ref={emailRef} type="text" placeholder="Email" />
+                        <input className="text-box" ref={passwordRef} type="password" placeholder="Password" />
+                        <button className="btn" type="submit">Login</button>
                     </form>
-                    <button className="btn">Login</button>
-                    <p className="text-form">Not register? <a href="#">Create an account</a></p>
+                    <p className="text-form">Not register? <a href="/register">Create an account</a></p>
                 </div>
             </div>
         </div>
